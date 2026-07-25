97f79c44c14148b9e904da7452666bf52239f91c
Mhamza7119 <muhammadhamzakch@gmail.com>
2026-07-25 10:32:12 -0700

    first assignment

---
+++ BASICS/LAB-01/code/variable.js
@@ -0,0 +1,24 @@
+
+//personal information
+const mynameis ="Muhammad hamza";
+
+
+const myageis =20;
+
+
+const imastudent =true;
+
+
+const myfavouriteprogramminglanguage ="javascript";
+
+
+//output
+
+console.log("name:", mynameis);
+console.log("age:", myageis);
+console.log("student:",imastudent);
+console.log("favourite:",myfavouriteprogramminglanguage);
+
+
+
+
