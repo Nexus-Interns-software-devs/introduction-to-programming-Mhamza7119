e6be7a32c88cad724ba7f8ce1ac7e499b80117fe
Mhamza7119 <muhammadhamzakch@gmail.com>
2026-07-25 11:36:29 -0700

    2assignment

---
+++ BASICS/TICKET-01/code/calculator.js
@@ -0,0 +1,17 @@
+//variable
+let num1 =50;
+let num2 =50;
+
+//operation
+let sum =num1  + num2;
+let difference =num1 - num2;
+let product =num1 * num2;
+let quotient =num1 / num2;
+
+
+//output 
+console.log("calculate result:");
+console.log(`Addition: ${num1} + ${num2} = ${sum}`);
+console.log(`subtraction: ${num1} - ${num2} = ${difference}`);
+console.log(`multiplication: ${num1} * ${num2} = ${product}`);
+console.log(`division: ${num1} / ${num2} = ${quotient}`);
