c95e78a534405044183c765bb48df4a3d17e6484
Mhamza7119 <muhammadhamzakch@gmail.com>
2026-07-27 09:22:22 -0700

    operators

---
+++ CONTROL STRUCTURE/LAB-02/code/operators.js
@@ -0,0 +1,28 @@
+//variable
+let num1 =70;
+let num2 =40;
+let equal = num1 == num2;
+
+//arithmatic 
+let sum =num1 + num2;
+let product =num1 * num2;
+let remainder =num1 % num2;
+
+console.log("sum:",sum);
+console.log("product:",product);
+console.log("remainder:",remainder);
+
+//comparison operation
+let isequal =num1 === num2;
+let isgreater= num1 > num2;
+
+console.log("is equal:",equal);
+console.log("is greater:",isgreater);
+
+
+//logical oparation
+let passedmath =true;
+let passedenglish =false;
+console.log("AND Result",passedmath && passedenglish);
+console.log("OR Result",passedmath || passedenglish);
+
